10c3a75f789ba7b047f5312db6fc67f96680fa67
chumarjamil <imumarj@gmail.com>
2024-11-18 10:23:56 +0700

    creating project directory stucture

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import "fmt"
 
 func main() {
-	// create instance of fiber
-	app := fiber.New()
-
-	// create http handler
-	app.Get("/testApi", func (ctx *fiber.Ctx) error {
-		return ctx.Status(200).JSON(fiber.Map{
-			"success" : "true",
-			"message" : "Here is my first API in Fiber",
-		})
-	})
-
-	// listen on port
-	app.Listen(":9090")
+	fmt.Println("Creating structure")
 }
