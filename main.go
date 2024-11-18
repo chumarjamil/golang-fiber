53bfe17a92261fa81bbefbd01deb6baa9f46d39c
chumarjamil <imumarj@gmail.com>
2024-11-18 13:35:29 +0700

    database code cleanup

--- main.go
+++ main.go
@@ -2,46 +2,11 @@ package main
 
 import (
 	"fmt"
-	models "go_lang_rest_api/model"
-	"os"
-
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	db "go_lang_rest_api/config"
 )
-
-var DB *gorm.DB
 
 func main() {
 	fmt.Println("Creating structure")
 
-	godotenv.Load()
-	dbname := os.Getenv("MYSQL_DBNAME")
-	dbuser := os.Getenv("MYSQL_USER")
-	dbpassword := os.Getenv("MYSQL_PASSWORD")
-	dbhost := os.Getenv("MYSQL_HOST")
-
-	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbuser, dbpassword, dbhost, dbname)
-  	var db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
-
-	if err != nil {
-		panic("DB connection failed")
-	}
-
-	DB = db
-	fmt.Println("Db connection successful!")
-
-	AutoMigrate(db)
-}
-
-func AutoMigrate(connection *gorm.DB) {
-	connection.Debug().AutoMigrate(
-		&models.Cashier{},
-		&models.Category{},
-		&models.Discount{},
-		&models.Order{},
-		&models.Payment{},
-		&models.PaymentType{},
-		&models.Product{},
-	)
+	db.Connect()
 }
